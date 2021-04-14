c222501589bc5b46ebe42400913081910a450626
Wojciech Rak <rak.wojtek@wp.pl>
2021-04-14 18:16:54 +0200

    Update Program.cs

--- SimpleWebApiApp/Program.cs
+++ SimpleWebApiApp/Program.cs
@@ -4,14 +4,5 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
 WebHost.CreateDefaultBuilder()
-        .Configure(app =>
-        {
-            // configure application pipeline
-            app.UseRouting();
-            app.UseEndpoints(e =>
-            {
-                e.MapGet("/", c => c.Response.WriteAsync("Hello world!"));
-                e.MapGet("hello/{name}", c => c.Response.WriteAsync($"Hello, {c.Request.RouteValues["name"]}"));
-            });
-        })
+        .Configure(app => app.Run(c => c.Response.WriteAsync("Hello world!")))
         .Build().Run();
